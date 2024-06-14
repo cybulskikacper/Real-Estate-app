71bc49c2f869c6ff04d1c1f2bf3f4f826763a59c
Kacper <kacpercybulski@icloud.com>
2024-06-14 17:24:29 +0200

    adding images

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,7 +7,38 @@ function Card({ item }) {
 			<Link to={`/${item.id}`} className="imageContainer">
 				<img src={item.img} alt="" />
 			</Link>
-			<div className="textContainer"></div>
+			<div className="textContainer">
+				<h2 className="title">
+					<Link to={`/${item.id}`}>{item.title}</Link>
+				</h2>
+				<p className="address">
+					<img src="./src/assets/pin.png" alt="" />
+					<span>{item.address}</span>
+				</p>
+
+				<p className="price">${item.price}</p>
+
+				<div className="bottom">
+					<div className="features">
+						<div className="feature">
+							<img src="./src/assets/bed.png" alt="" />
+							<span>{item.bedroom} bedroom</span>
+						</div>
+						<div className="feature">
+							<img src="./src/assets/bath.png" alt="" />
+							<span>{item.bathroom} bathroom</span>
+						</div>
+					</div>
+					<div className="icons">
+						<div className="icon">
+							<img src="./src/assets/save.png" alt="" />
+						</div>
+						<div className="icon">
+							<img src="./src/assets/chat.png" alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
