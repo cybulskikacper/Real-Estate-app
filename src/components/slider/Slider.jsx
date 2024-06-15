a49f4aea44a42d9e784fb85f5e58be9fa48e5a4d
Kacper <kacpercybulski@icloud.com>
2024-06-15 16:21:27 +0200

    working on slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,7 +1,30 @@
 import './slider.scss'
 
-function Slider() {
-	return <h1>Slider</h1>
+function Slider({ images }) {
+	return (
+		<div className="slider">
+			<div className="fullSlider">
+				<div className="arrow">
+					<img src="./src/assets/arrow.png" alt="" />
+				</div>
+				<div className="imgContainer">
+					<img src={images[0]} alt="" />
+				</div>
+				<div className="arrow">
+					<img src="./src/assets/arrow.png" alt="" />
+				</div>
+			</div>
+
+			<div className="bigImage">
+				<img src={images[0]} alt="" />
+			</div>
+			<div className="smallImages">
+				{images.slice(1).map((image, index) => (
+					<img src={image} alt="" key={index}></img>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default Slider
